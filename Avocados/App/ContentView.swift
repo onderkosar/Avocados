c7003af1501d4955143ad54fb8e1ce547b983fb6
Önder Koşar <onderkosar@hotmail.com>
2020-10-15 15:54:07 +0300

    header view is designed

--- Avocados/App/ContentView.swift
+++ Avocados/App/ContentView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Avocado Recipes")
-            .padding()
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // MARK: - HEADER
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0) {
+                        HeaderView()
+                    }
+                }
+                
+                // MARK: - FOOTER
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    Text("Everything you wanted to know about avocados but were too afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                } //: FOOTER
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            } //: VSTACK
+        } //: SCROLLVİEW
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
+// MARK: -PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
